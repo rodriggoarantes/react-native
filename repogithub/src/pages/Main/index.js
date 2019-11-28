f100e617df11085041735d30ba9daedacc2202dd
rodrigo <rodrigo.arantes@totvs.com.br>
2019-11-28 19:10:07 -0300

    #repogithub adicionando usuarios com hooks e asyncstorage

--- repogithub/src/pages/Main/index.js
+++ repogithub/src/pages/Main/index.js
@@ -1,41 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import { Alert, Keyboard } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Alert, Keyboard, ActivityIndicator, AsyncStorage } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default function Main() {
+  const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState('');
 
   useEffect(() => {
     async function loadStart() {
-      setUsers([...users]);
+      setUsers([]);
     }
     loadStart();
   }, []);
 
-  async function handleSubmit() {
-    try {
-      const response = await api.get(`/users/${newUser}`);
+  useEffect(() => {
+    AsyncStorage.setItem('users', JSON.stringify(users));
+  }, [users]);
 
-      const data = {
-        name: response.data.name,
-        login: response.data.login,
-        bio: response.data.bio,
-        avatar: response.data.avatar_url,
-      };
+  const handleSubmit = useCallback(async () => {
+    if (newUser && newUser !== '') {
+      const exist = users.filter(u => u && u.login && u.login === newUser)
+        .length;
 
-      setUsers([...users, data]);
+      if (exist > 0) {
+        Alert.alert(
+          'Usuario Existente',
+          'Usuario informado já existe na listagem'
+        );
+        return;
+      }
 
-      Alert.alert('Usuario', 'Usuario adicionado com sucesso');
-      Keyboard.dismiss();
-    } catch (error) {
-      Alert.alert('Usuario', 'Erro ao adicionar o usuario');
+      setLoading(true);
+
+      try {
+        const response = await api.get(`/users/${newUser}`);
+
+        if (response.data && response.data.name) {
+          const data = {
+            name: response.data.name || '<Não Informado>',
+            login: response.data.login,
+            bio: response.data.bio || '<Não Informado>',
+            avatar: response.data.avatar_url,
+          };
+
+          setUsers([...users, data]);
+          setLoading(false);
+
+          Alert.alert('Usuario', 'Usuario adicionado com sucesso');
+          Keyboard.dismiss();
+        } else {
+          setLoading(false);
+          Alert.alert('Erro', 'Usuario NAO encontrado');
+        }
+
+        setNewUser('');
+      } catch (error) {
+        Alert.alert('Erro', 'Erro ao adicionar o usuario');
+      } finally {
+        setLoading(false);
+      }
     }
-  }
+  }, [users, newUser]);
 
   return (
     <>
@@ -50,10 +91,29 @@ export default function Main() {
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
           />
-          <SubmitButton onPress={handleSubmit}>
-            <Icon name="add" size={20} color="#FFF" />
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#FFF" />
+            )}
           </SubmitButton>
         </Form>
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     </>
   );
