9300117c97b196f8941f4c686677e973d1dc15ae
rodrigo <rodrigo.arantes@totvs.com.br>
2019-11-29 14:33:21 -0300

    #repogithub removendo config de redux desnecessaria

--- repogithub/src/store/modules/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import home from './home/reducer';
-
-export default combineReducers({
-  home,
-});
--- repogithub/src/store/modules/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all } from 'redux-saga/effects';
-import home from './home/sagas';
-
-export default function* rootSaga() {
-  return yield all([home]);
-}
