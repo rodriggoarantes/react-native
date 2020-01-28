9e2c16bc64dc8ec2e5643b61fab7385905453d58
rodrigo <rodrigo.arantes@totvs.com.br>
2020-01-28 14:11:21 -0300

    #devradar configuração inicial do projeto

---
+++ devradar/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react-native/no-raw-text': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'global-require': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+};
---
+++ devradar/src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from '~/components/Container';
+
+export default function Main() {
+  return (
+    <Container>
+      <Text>Hello Componente</Text>
+    </Container>
+  );
+}
