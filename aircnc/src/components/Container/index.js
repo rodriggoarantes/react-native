dcbc3c036307b4ea25e5d1a1f787dfce1dab88e6
rodrigo <rodrigo.arantes@totvs.com.br>
2020-03-02 14:35:56 -0300

    #aircnc tela de login

--- aircnc/src/components/Container/index.js
+++ aircnc/src/components/Container/index.js
@@ -1,5 +1,12 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: Platform.OS === 'ios' ? 'padding' : 'height',
+})`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
 `;
