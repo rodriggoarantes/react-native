9dcfbab872c4339a5dd9ac9388c4e2cc8f3ef4ff
rodrigo <rodrigo.arantes@totvs.com.br>
2020-03-03 13:41:11 -0300

    #aircnc icones e mecanismo de login

--- aircnc/src/pages/Login/index.js
+++ aircnc/src/pages/Login/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Image, AsyncStorage, Alert } from 'react-native';
 import { Container } from '~/components/Container';
 import { Label, Form, Input, Button, ButtonText } from './styles';
 
+import api from '~/services/api';
+
 import logo from '~/assets/logo.png';
 
-export default function Login() {
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Spots');
+      }
+    });
+  }, []);
+
+  const handleSubmit = async () => {
+    if (!email || !techs) {
+      Alert.alert('Atenção', 'E-mail e Tecnologias são obrigatórios.');
+      return;
+    }
+
+    const user = await api.post('/sessions', { email });
+    if (user && user.data) {
+      const { id } = user.data;
+
+      await AsyncStorage.setItem('user', id);
+      await AsyncStorage.setItem('techs', techs);
+
+      Alert.alert('OK', 'Usuário logado com sucesso.');
+      navigation.navigate('Spots');
+    } else {
+      Alert.alert('Atenção', 'Usuário não encontrado.');
+    }
+  };
+
   return (
     <Container>
       <Image source={logo} />
@@ -17,6 +50,7 @@ export default function Login() {
           keyboardType="email-address"
           autoCapitalize="none"
           autoCorrect={false}
+          onChangeText={text => setEmail(text)}
         />
 
         <Label>TECNOLOGIAS *</Label>
@@ -25,9 +59,10 @@ export default function Login() {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
+          onChangeText={text => setTechs(text)}
         />
 
-        <Button>
+        <Button onPress={handleSubmit}>
           <ButtonText>Encontrar spots</ButtonText>
         </Button>
       </Form>
