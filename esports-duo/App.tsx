971a0e3d32ff683790f5f0078b66a062f8e62591
Rodrigo Arantes <rodriggoarantes@gmail.com>
2022-09-15 13:55:30 -0300

    #reactnative #esports primeiros componentes

--- esports-duo/App.tsx
+++ esports-duo/App.tsx
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>eSports - NLW 09/2022</Text>
+      
+      <Text style={styles.title}>eSports - NLW 09/2022</Text>
+
+      <Button title='Botao Opacity A'/>
+      <Button title='Hello'/>
+      <Button title='World'/>
+
       <StatusBar style="auto" />
+
     </View>
   );
+}
+
+function Button (props: ButtonProps) {
+  const { title } = props;
+  return (
+    <TouchableOpacity>
+      <Text style={styles.btnTitle}>{ title }</Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +37,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#f00',
+    fontSize: 22
+  },
+  btnTitle: {
+    color: '#00F',
+    fontSize: 22,
+    borderColor: '#000'
+  }
 });
