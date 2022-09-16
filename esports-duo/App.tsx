9a7d86105dba5bcdcd0a5426f57e7290f751139c
Rodrigo Arantes <rodriggoarantes@gmail.com>
2022-09-16 15:05:29 -0300

    #reactnative #esports home

--- esports-duo/App.tsx
+++ esports-duo/App.tsx
@@ -1,10 +1,31 @@
-import { View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
+
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+
+import { Home } from './src/screens/Home';
 
 export default function App() {
-  return (
-    <View>
-      
 
-    </View>
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+      <Background>
+        <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+
+        { fontsLoaded ? <Home /> : <Loading /> }
+      </Background>
   );
 }
---
+++ esports-duo/src/screens/Home/index.tsx
@@ -0,0 +1,36 @@
+import { Image, Text, View, FlatList } from 'react-native';
+
+import logo from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+
+import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+        <Image source={logo} style={styles.logo}/>
+
+        <Heading 
+            title='Encontro o seu duo!' 
+            subtitle='Selecione o game que deseja jogar...' />
+
+        <FlatList
+          data={GAMES}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <GameCard 
+                  data={item}
+                />
+            )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.contentList}
+        />
+                
+        <Text>APP eSports</Text>
+    </View>
+  );
+}
